788b99cbbdbd6c381483835217d5325e73f751c4
Jared Lunde <jared.lunde@gmail.com>
2020-08-19 21:13:03 -0600

    feat: do initial work

--- README.md
+++ README.md
@@ -9,14 +9,8 @@ npm i @proser/babel-plugin-reading-time
 ```
 
 <p>
-  <a href="https://bundlephobia.com/result?p=@proser/babel-plugin-reading-time">
-    <img alt="Bundlephobia" src="https://img.shields.io/bundlephobia/minzip/@proser/babel-plugin-reading-time?style=for-the-badge&labelColor=24292e">
-  </a>
   <a aria-label="Types" href="https://www.npmjs.com/package/@proser/babel-plugin-reading-time">
     <img alt="Types" src="https://img.shields.io/npm/types/@proser/babel-plugin-reading-time?style=for-the-badge&labelColor=24292e">
-  </a>
-  <a aria-label="Code coverage report" href="https://codecov.io/gh/proser-js/babel-plugin-reading-time">
-    <img alt="Code coverage" src="https://img.shields.io/codecov/c/gh/proser-js/babel-plugin-reading-time?style=for-the-badge&labelColor=24292e">
   </a>
   <a aria-label="Build status" href="https://travis-ci.com/proser-js/babel-plugin-reading-time">
     <img alt="Build status" src="https://img.shields.io/travis/com/proser-js/babel-plugin-reading-time?style=for-the-badge&labelColor=24292e">
@@ -33,19 +27,13 @@ npm i @proser/babel-plugin-reading-time
 
 ## Quick start
 
-```js
-import _ from '@proser/babel-plugin-reading-time'
+```json
+{
+  "proser": {
+    "plugins": ["@proser/babel-plugin-reading-time"]
+  }
+}
 ```
-
-## API
-
-### someFunction()
-
-#### Arguments
-
-| Name | Type | Default | Required? | Description |
-| ---- | ---- | ------- | --------- | ----------- |
-|      |      |         |           |             |
 
 ## LICENSE
 
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,70 @@
-export default 'hello world'
+import * as types from 'babel-types'
+import readingTime from 'reading-time'
+import {readFileCache} from 'proser/bin'
+import type {ProserBabelPluginOptions} from 'proser/bin'
+
+module.exports = function babelPluginReadingTime({
+  types: t,
+}: {
+  types: typeof types
+}) {
+  return {
+    name: '@proser/babel-plugin-reading-time',
+    visitor: {
+      ExportDeclaration(
+        nodePath: any,
+        state: {opts: ProserBabelPluginOptions}
+      ) {
+        const {posts} = state.opts
+        const postContents = posts.reduce((acc, post) => {
+          acc[post.slug] = readFileCache(post.filepath)
+          return acc
+        }, {} as Record<string, string>)
+        const postsDeclarator = (
+          nodePath.node.declaration.declarations || []
+        ).find((declarator: any) => declarator.id.name === 'postsMap')
+        if (!postsDeclarator) return
+
+        const currentPosts = postsDeclarator.init.properties
+
+        for (const i in currentPosts) {
+          // Replace the current `component` property with the latest
+          // one
+          const properties = currentPosts[i].value.properties
+          const slug = properties.find(
+            (prop: any) => prop.key.name === 'slug' || prop.key.value === 'slug'
+          ).value.value
+          const metadataProperties = properties.find(
+            (prop: any) =>
+              prop.key.name === 'metadata' || prop.key.value === 'metadata'
+          ).value.properties
+          const {text, time, words} = readingTime(postContents[slug])
+          const readingTimeExpression = t.objectExpression([
+            t.objectProperty(t.identifier('text'), t.stringLiteral(text)),
+            t.objectProperty(
+              t.identifier('time'),
+              t.numericLiteral(Math.round(time))
+            ),
+            t.objectProperty(t.identifier('words'), t.numericLiteral(words)),
+          ])
+          const readingTimeProperty = metadataProperties.find(
+            (prop: any) =>
+              prop.key.name === 'readingTime' ||
+              prop.key.value === 'readingTime'
+          )
+          // Set reading time property
+          if (readingTimeProperty) {
+            readingTimeProperty.value = readingTimeExpression
+          } else {
+            metadataProperties.push(
+              t.objectProperty(
+                t.stringLiteral('readingTime'),
+                readingTimeExpression
+              )
+            )
+          }
+        }
+      },
+    },
+  }
+}
